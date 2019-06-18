126bc004b052b518015f72589a2389d93f730a3a
layla dantas <laylatdantas@gmail.com>
2019-06-18 03:51:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram 
+ attr_accessor :word 
+ 
+ def initialize(word)
+   @word = word 
+   
+ end 
+ 
+ def match(array)
+  match_word = @word.split("").sort
+   result = []
+   array.each do |item|
+     match_item = item.split("").sort
+     if match_word==match_item
+      result.push(item)
+    end
+   end
+   result
+ end
+ 
+ 
+end  
+   

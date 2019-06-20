23b007938f5e18c2552a2144d3bdb4a974ab510f
Jack Atherton <jackrrye@gmail.com>
2019-06-20 21:04:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+    @anagram_array = []
+  end 
+  
+  def match(array_of_anagrams)
+    array_of_anagrams.collect do |anagram| 
+      if anagram.split("").sort == @word.split("").sort 
+        anagram 
+      else 
+        nil
+      end 
+    end.compact
+  end 
+  
+end
+  

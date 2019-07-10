631dbaca88cbd3e6c2a539d936790f7ab7078215
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-10 02:52:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :input
+  
+  def initialize(input)
+    @input = input
+  end
+  
+  def match(possible_anagrams)
+    split_input = input.split("")
+    
+    possible_anagrams.select {|word| word.split("").sort == split_input.sort}
+  end
+end 

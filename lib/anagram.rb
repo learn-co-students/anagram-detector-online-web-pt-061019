8dd150a9c7d2cf7db4e5e093b5d7ac3826384603
Lantz Warrick <betalantz@gmail.com>
2019-07-11 05:09:29 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.map {|w|
+      w if w.split("").sort == @word.split("").sort
+    }.compact
+  end
+
+end

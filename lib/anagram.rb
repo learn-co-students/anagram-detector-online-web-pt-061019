d05f2b96a2a641f5e7dc3dffa5ea50f2956d9bb1
Aida Abdikulova <abdikulova@gmail.com>
2019-07-19 04:48:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,25 +4,29 @@ class Anagram
   attr_accessor :word_match
   @@all = []
 
-  def initalize(word)
+  def initialize(word)
     @word_match = word #this is essentially saying that, when I create a new instance, I will give myself the ability to input a string/data/integer and set it equal to the instance.
     @@all << self
   end
 
   def match(input)
-    array = input.split(" ") #self is referring to the instance in this case and usually whatever method or whatever scope it's in.
-      array.each do |values|
-        values.sort!
-        return array
-  end
-    self.word_match.sort!
-    if array == self.word_match
-      return array
-    else
-      return []
+     #self is referring to the instance in this case and usually whatever method or whatever scope it's in.
+    array = []
+    input.each do |values|
+        return_value = values.chars.sort.join
+        if return_value == self.word_match.chars.sort.join
+          array << values
     end
   end
+    return array
+  end
 
-end
+    #self.word_match.chars.sort.join
 
+    #if ## == self.word_match.chars.sort.join
+    #  return input
+    #else
+      #return []
+    #end
+  #end
 end

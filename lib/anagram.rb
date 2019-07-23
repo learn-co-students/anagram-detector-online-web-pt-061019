caffe326c981308fc8b942466256bf2c9781d922
Rene Cocom <fasnimo@gmail.com>
2019-07-23 20:16:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(w_array)
+    w_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end 
+end 
